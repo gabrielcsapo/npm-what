b63640bbf374b2d8ac67eba932cc94ab886eb343
Gabriel Csapo <gabecsapo@gmail.com>
2016-06-11 20:53:44 -0700

    I think we got all the information we need { total_code: '22614',   stats: 'https://github.com/AlDanial/cloc v 1.66  T=0.42 s (118.1 files/s, 54021.1 lines/s)',   languages:    [ 'JSON                             4              0              0          15906',      'CSS                              4              0             13           2192',      'LESS                            16             36             44           1782',      'SASS                            14             34             34           1656',      'JavaScript                      10             85             10           1012',      'HTML                             1              0              0             63',      'YAML                             1              0              0              3' ],   modules:    [ 'babel',      'babel-core',      'babel-preset-es2015',      'bluebird',      'body-parser',      'chartist',      'codemirror',      'compression',      'express',      'figlet',      'filesize',      'font-awesome',      'fs-extra',      'js-beautify',      'lowdb',      'moment',      'psychic-ui',      'pug',      'serve-static' ] }

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
+var fs = require('fs');
+var path = require('path');
 var exec = require('child_process').exec;
-var cmd = './node_modules/.bin/cloc /Users/gabrielcsapo/Documents/node-notebook --exclude-dir=node_modules';
-
+var directory = process.env.DIRECTORY || process.cwd();
+console.log(directory);
+var cmd = './node_modules/.bin/cloc ' + directory + ' --exclude-dir=node_modules';
+console.log(cmd);
+// yeah I stole this from `./node_modules/.bin/cloc --show-lang`
+var supported_languages = ["BAP","ActionScript","Ada","ADSO/IDSM","AMPLE","Ant","Apex Trigger","Arduino Sketch","ASP","ASP.Net","AspectJ","Assembly","AutoHotkey","awk","Bourne Again Shell","Bourne Shell","C","C Shell","C#","C++","C/C++ Header","CCS","Clojure","ClojureC","ClojureScript","CMake","COBOL","CoffeeScript","ColdFusion","ColdFusion CFScript","Coq","Crystal","CSON","CSS","CUDA","Cython","D","DAL","Dart","diff","DITA","DOORS Extension Language","DOS Batch","DTD","dtrace","ECPP","EEx","Elixir","Elm","ERB","Erlang","Expect","F#","Focus","Forth","Fortran 77","Fortran 90","Fortran 95","GDScript","Go","Grails","Groovy","Haml","Handlebars","Harbour","Haskell","HLSL","HTML","IDL","InstallShield","Java","JavaScript","JavaServer Faces","JCL","JSON","JSP","Julia","Kermit","Korn Shell","Kotlin","LESS","lex","Lisp","LiveLink OScript","Lua","m4","make","MATLAB","Maven","Modula3","MSBuild script","MUMPS","Mustache","MXML","NAnt script","NASTRAN DMAP","Nemerle","Objective C","Objective C++","OCaml","OpenCL","Oracle Forms","Oracle Reports","Pascal","Pascal/Puppet","Patran Command Language","Perl","PHP","PHP/Pascal","Pig Latin","PL/I","PowerBuilder","PowerShell","Prolog","Protocol Buffers","PureScript","Python","QML","Qt","Qt Project","R","Racket","Razor","Rexx","RobotFramework","Ruby","Ruby HTML","Rust","SAS","SASS","Scala","sed","SKILL","SKILL++","Smarty","Softbridge Basic","SQL","SQL Data","SQL Stored Procedure","Standard ML","Stylus","Swift","Tcl/Tk","Teamcenter met","Teamcenter mth","Titanium Style Sheet","Twig","TypeScript","Unity-Prefab","Vala","Vala Header","Velocity Template Language","Verilog-SystemVerilog","VHDL","vim script","Visual Basic","Visual Fox Pro","Visualforce Component","Visualforce Page","Windows Message File","Windows Module Definition","Windows Resource File","WiX include","WiX source","WiX string localization","XAML","xBase","xBase Header","XHTML","XMI","XML","XQuery","XSD","XSLT","yacc","YAML","zsh"];
+// we will use this eventually
+var output = {
+    total_code: "",
+    stats: "",
+    languages: [],
+    modules: []
+};
 exec(cmd, function(error, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    console.log(stdout.split('\n'));
+    // Lets get the total amount of code this project uses
+    stdout.split('\n').forEach(function(out) {
+        if(out.indexOf('files/s') > -1) {
+            return output.stats = out;
+        }
+        if (supported_languages.some(function(v) { return out.indexOf(v) >= 0; })) {
+            // hey this is a language line, lets store this for later
+            output.languages.push(out);
+        }
+        if(out.indexOf('SUM') > -1) {
+            var lc = [];
+            var done = false;
+            var line = out.split('');
+            line.reverse();
+            line.forEach(function(c) {
+                if(c !== ' ') {
+                    if(!done) {
+                        lc.push(c);
+                    }
+                } else {
+                    return done = true;
+                }
+            });
+            output.total_code = lc.reverse().join('');
+        }
+    });
+    var package = JSON.parse(fs.readFileSync(path.resolve(directory, 'package.json')));
+    output.modules = Object.keys(package.dependencies);
+    console.log(output);
 });
